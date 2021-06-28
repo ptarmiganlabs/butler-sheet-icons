1934e0ccae7789539debe65eee044587150b7229
Göran Sander <goran@ptarmiganlabs.com>
2021-06-28 23:31:25 +0200

    fix: improved info texts in app

--- src/qrs.js
+++ src/qrs.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { logger } = require('./globals.js');
 
 const setupQseowQrsConnection = options => {
-  logger.debug('Prepping for QSEoW QRS connection...');
+  logger.debug('Setting up connection to QSEoW QRS...');
 
   // Set up QSEoW repository service configuration
   // Always connect directly to QRS, i.e. with virtual proxy ''
