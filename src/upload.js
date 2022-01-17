5936038f2b6e921ffa5176cb06b69f43b31a2a10
Göran Sander <goran@ptarmiganlabs.com>
2022-01-17 06:00:40 +0100

    refactor: Better source code formatting
    Implements #98 - Align source formatting with other Butler tools

--- src/upload.js
+++ src/upload.js
@@ -1,75 +1,77 @@
-'use strict';
-
-var fs = require('fs');
-var path = require('path');
-var qrsInteract = require('qrs-interact');
+/* eslint-disable import/extensions */
+const fs = require('fs');
+const path = require('path');
+const qrsInteract = require('qrs-interact');
 
 const { logger, setLoggingLevel } = require('./globals.js');
 const { setupQseowQrsConnection } = require('./qrs.js');
 
 /**
- * 
- * @param {*} options 
+ *
+ * @param {*} options
  */
-const qseowUploadToContentLibrary = async options => {
-  try {
-    // Set log level
-    setLoggingLevel(options.loglevel);
+const qseowUploadToContentLibrary = async (options) => {
+    try {
+        // Set log level
+        setLoggingLevel(options.loglevel);
 
-    const qseowConfigQrs = setupQseowQrsConnection(options);
-    const qrsInteractInstance = new qrsInteract(qseowConfigQrs);
+        const qseowConfigQrs = setupQseowQrsConnection(options);
+        // eslint-disable-next-line new-cap
+        const qrsInteractInstance = new qrsInteract(qseowConfigQrs);
 
-    logger.debug('QSEoW QRS config: ' + JSON.stringify(qseowConfigQrs, null, 2));
+        logger.debug(`QSEoW QRS config: ${JSON.stringify(qseowConfigQrs, null, 2)}`);
 
-    const iconFolderAbsolute = path.resolve(options.imagedir);
-    const contentlibrary = options.contentlibrary;
+        const iconFolderAbsolute = path.resolve(options.imagedir);
+        const { contentlibrary } = options;
 
-    logger.info('Uploading images in folder: ' + iconFolderAbsolute);
-    logger.info('Uploading images to Qlik Sense content library: ' + contentlibrary);
+        logger.info(`Uploading images in folder: ${iconFolderAbsolute}`);
+        logger.info(`Uploading images to Qlik Sense content library: ${contentlibrary}`);
 
-    let files = fs.readdirSync(iconFolderAbsolute);
+        const files = fs.readdirSync(iconFolderAbsolute);
 
-    for (const file of files) {
-      // Get complete path for file
-      let fileFullPath = path.join(iconFolderAbsolute, file);
-      let fileStat = fs.statSync(fileFullPath);
+        // eslint-disable-next-line no-restricted-syntax
+        for (const file of files) {
+            // Get complete path for file
+            const fileFullPath = path.join(iconFolderAbsolute, file);
+            const fileStat = fs.statSync(fileFullPath);
 
-      if (fileStat.isFile() && file.substring(0, 4) === 'app-' && path.extname(file) === '.png') {
-        logger.verbose(`Uploading file: ${file}`);
+            if (
+                fileStat.isFile() &&
+                file.substring(0, 4) === 'app-' &&
+                path.extname(file) === '.png'
+            ) {
+                logger.verbose(`Uploading file: ${file}`);
 
-        var apiUrl =
-          '/contentlibrary/' +
-          encodeURIComponent(contentlibrary) +
-          '/uploadfile?externalpath=' +
-          file +
-          '&overwrite=true';
+                const apiUrl = `/contentlibrary/${encodeURIComponent(
+                    contentlibrary
+                )}/uploadfile?externalpath=${file}&overwrite=true`;
 
-        logger.debug('Thumbnail imague upload URL: ' + apiUrl);
+                logger.debug(`Thumbnail imague upload URL: ${apiUrl}`);
 
-        try {
-          let fileData = fs.readFileSync(fileFullPath);
-          qrsInteractInstance
-            .Post(apiUrl, fileData, 'image/png')
-            .then(result => {
-              logger.debug('QSEoW image upload result=' + JSON.stringify(result));
-              logger.verbose('QSEoW image upload done: ' + file);
-            })
-            .catch(err => {
-              // Return error msg
-              logger.error('Error uploading icon to content library: ' + err);
-            });
-        } catch (err) {
-          logger.error(`UPLOAD 1: ${JSON.stringify(err, null, 2)}`);
+                try {
+                    const fileData = fs.readFileSync(fileFullPath);
+                    qrsInteractInstance
+                        .Post(apiUrl, fileData, 'image/png')
+                        .then((result) => {
+                            logger.debug(`QSEoW image upload result=${JSON.stringify(result)}`);
+                            logger.verbose(`QSEoW image upload done: ${file}`);
+                        })
+                        .catch((err) => {
+                            // Return error msg
+                            logger.error(`Error uploading icon to content library: ${err}`);
+                        });
+                } catch (err) {
+                    logger.error(`UPLOAD 1: ${JSON.stringify(err, null, 2)}`);
+                }
+            } else if (fileStat.isDirectory()) {
+                logger.verbose(`${fileFullPath} is a directory, skipping.`);
+            }
         }
-      } else if (fileStat.isDirectory()) {
-        logger.verbose(fileFullPath + ' is a directory, skipping.');
-      }
+    } catch (err) {
+        logger.error(`UPLOAD 2: ${JSON.stringify(err, null, 2)}`);
     }
-  } catch (err) {
-    logger.error(`UPLOAD 2: ${JSON.stringify(err, null, 2)}`);
-  }
 };
 
 module.exports = {
-  qseowUploadToContentLibrary,
+    qseowUploadToContentLibrary,
 };
