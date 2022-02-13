07b398265351fa11654e6a3111d8185bf5bfe588
Göran Sander <goran@ptarmiganlabs.com>
2022-02-13 21:11:32 +0100

    refactor: Clean up code used to talk to QS Cloud APIs

--- src/lib/cloud/cloud-repo.js
+++ src/lib/cloud/cloud-repo.js
@@ -9,44 +9,44 @@ const qlikSaas = function QlikSaas(config) {
 
     config.baseURL = `https://${config.url}`;
 
-    this.Get = async function (path) {
+    this.Get = async (path) => {
         if (!path) throw Error({ message: `"path" parameter is missing` });
 
         return request(config, path, 'get');
     };
 
-    this.Delete = async function (path) {
+    this.Delete = async (path) => {
         if (!path) throw Error({ message: `"path" parameter is missing` });
 
         return request(config, path, 'delete');
     };
 
-    this.Patch = async function ({
+    this.Patch = async ({
         path,
         data = {},
         contentType = 'application/json',
         file = '',
         fileName = '',
-    }) {
+    }) => {
         if (!path) throw Error({ message: `"path" parameter is missing` });
         if (!data && !file) throw Error({ message: `"data" and/or "file" parameter is missing` });
 
         return request(config, path, 'patch', data, contentType, file, fileName);
     };
 
-    this.Post = async function ({
+    this.Post = async ({
         path,
         data = {},
         contentType = 'application/json',
         file = '',
         fileName = '',
-    }) {
+    }) => {
         if (!path) throw Error({ message: `"path" parameter is missing` });
 
         return request(config, path, 'post', data, contentType, file, fileName);
     };
 
-    this.Put = async function ({ path, data = {}, contentType = 'application/json', file = '' }) {
+    this.Put = async ({ path, data = {}, contentType = 'application/json', file = '' }) => {
         if (!path) throw Error({ message: `"path" parameter is missing` });
 
         return request(config, path, 'put', data, contentType, file);
