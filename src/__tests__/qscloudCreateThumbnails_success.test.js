7d655e89611b54c2f39709089f95612dc63b686d
Göran Sander <mountaindude@ptarmiganlabs.com>
2022-10-02 06:32:20 +0000

    fix: Automated tests for Qlik Cloud work again

--- src/__tests__/qscloudCreateThumbnails_success.test.js
+++ src/__tests__/qscloudCreateThumbnails_success.test.js
@@ -1,33 +1,31 @@
-// Commented out due to expired access to Qlik Cloud tenant
+const { qscloudCreateThumbnails } = require('../lib/cloud/cloud-create-thumbnails');
 
-// const { qscloudCreateThumbnails } = require('../lib/cloud/cloud-create-thumbnails');
+const defaultTestTimeout = process.env.BSI_TEST_TIMEOUT || 600000; // 10 minute default timeout
 
-// const defaultTestTimeout = process.env.BSI_TEST_TIMEOUT || 600000; // 10 minute default timeout
+console.log(`Jest timeout: ${defaultTestTimeout}`);
 
-// console.log(`Jest timeout: ${defaultTestTimeout}`);
+const options = {
+    loglevel: process.env.BSI_LOG_LEVEL || 'debug',
+    tenanturl: process.env.BSI_CLOUD_TENANT_URL,
+    apikey: process.env.BSI_CLOUD_API_KEY,
+    logonuserid: process.env.BSI_CLOUD_LOGON_USERID,
+    logonpwd: process.env.BSI_CLOUD_LOGON_PWD,
+    collectionid: process.env.BSI_CLOUD_COLLECTION_ID,
+    headless: process.env.BSI_HEADLESS || 'true',
+    pagewait: process.env.BSI_PAGE_WAIT || '10',
+    imagedir: process.env.BSI_IMAGE_DIR || 'img',
+    schemaversion: process.env.BSI_CLOUD_SCHEMA_VERSION || '12.612.0',
+    appid: process.env.BSI_CLOUD_APP_ID,
+    includesheetpart: process.env.BSI_INCLUDE_SHEET_PART || 1,
+};
 
-// const options = {
-//     loglevel: process.env.BSI_LOG_LEVEL || 'debug',
-//     tenanturl: process.env.BSI_CLOUD_TENANT_URL,
-//     apikey: process.env.BSI_CLOUD_API_KEY,
-//     logonuserid: process.env.BSI_CLOUD_LOGON_USERID,
-//     logonpwd: process.env.BSI_CLOUD_LOGON_PWD,
-//     collectionid: process.env.BSI_CLOUD_COLLECTION_ID,
-//     headless: process.env.BSI_HEADLESS || 'true',
-//     pagewait: process.env.BSI_PAGE_WAIT || '10',
-//     imagedir: process.env.BSI_IMAGE_DIR || 'img',
-//     schemaversion: process.env.BSI_CLOUD_SCHEMA_VERSION || '12.612.0',
-//     appid: process.env.BSI_CLOUD_APP_ID,
-//     includesheetpart: process.env.BSI_INCLUDE_SHEET_PART || 1,
-// };
+jest.setTimeout(defaultTestTimeout);
 
-// jest.setTimeout(defaultTestTimeout);
-
-// /**
-//  * Create thumbnails with proper parameters
-//  * Should succeed
-//  */
-// test('qs cloud create sheet thumbnails, correct parameters (should succeed)', async () => {
-//     const data = await qscloudCreateThumbnails(options);
-//     expect(data).toBe(true);
-// });
+/**
+ * Create thumbnails with proper parameters
+ * Should succeed
+ */
+test('qs cloud create sheet thumbnails, correct parameters (should succeed)', async () => {
+    const data = await qscloudCreateThumbnails(options);
+    expect(data).toBe(true);
+});
