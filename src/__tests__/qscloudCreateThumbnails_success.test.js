8f2abb249c949642f6f0b0db9907097ca6a384ab
Göran Sander <mountaindude@ptarmiganlabs.com>
2024-02-14 09:09:53 +0000

    build(cloud): Switch to verbose logging for QS Cloud test cases ...

--- src/__tests__/qscloudCreateThumbnails_success.test.js
+++ src/__tests__/qscloudCreateThumbnails_success.test.js
@@ -8,7 +8,7 @@ const defaultTestTimeout = process.env.BSI_TEST_TIMEOUT || 1200000; // 20 minute
 console.log(`Jest timeout: ${defaultTestTimeout}`);
 
 const options = {
-    loglevel: process.env.BSI_LOG_LEVEL || 'debug',
+    loglevel: process.env.BSI_LOG_LEVEL || 'verbose',
     tenanturl: process.env.BSI_CLOUD_TENANT_URL,
     apikey: process.env.BSI_CLOUD_API_KEY,
     logonuserid: process.env.BSI_CLOUD_LOGON_USERID,
