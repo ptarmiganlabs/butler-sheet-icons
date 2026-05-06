575c2b786f57c42c28b330e432348d32eea54b13
Göran Sander <goran@ptarmiganlabs.com>
2026-05-06 11:05:10 +0200

    update npm install commands to include dev dependencies and adjust postject command usage

--- scripts/insider-build-mac.sh
+++ scripts/insider-build-mac.sh
@@ -16,13 +16,13 @@ mkdir -p ./build
 node --experimental-sea-config build-script/sea-config.json
 
 # Get a copy of the Node executable
-cp $(command -v node) ${DIST_FILE_NAME}
+cp "$(node -p 'process.execPath')" ${DIST_FILE_NAME}
 
 # Remove the signature from the Node executable
 codesign --remove-signature ${DIST_FILE_NAME}
 
 # Inject the blob
-./node_modules/.bin/postject ${DIST_FILE_NAME} NODE_SEA_BLOB ./build/sea-prep.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2 --macho-segment-name NODE_SEA
+npx postject ${DIST_FILE_NAME} NODE_SEA_BLOB ./build/sea-prep.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2 --macho-segment-name NODE_SEA
 
 mapfile -t ORIGINAL_KEYCHAINS < <(security list-keychains -d user | tr -d '"' | xargs -n1 || true)
 ORIGINAL_DEFAULT_KEYCHAIN=$(security default-keychain -d user | tr -d '"' | xargs || true)
