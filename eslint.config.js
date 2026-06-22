598fbf6700f3c7bbf1af2635cc191267858a005b
Göran Sander <goran@ptarmiganlabs.com>
2026-06-22 22:05:17 +0200

    update ESLint configuration and dependencies for improved compatibility

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,9 @@
 import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier/flat';
 import globals from 'globals';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all,
-});
 
 export default [
-    ...compat.extends('prettier'),
+    prettierConfig,
     {
         plugins: {
             prettier,
